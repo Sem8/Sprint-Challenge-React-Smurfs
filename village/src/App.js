c4806b1fdb4c19f0a8586fd40a820c1240ea6a7a
Sem8 <mssemmi8@gmail.com>
2019-03-01 11:40:03 -0500

    first goal of stage 1 of MVP done

--- village/src/App.js
+++ village/src/App.js
@@ -4,16 +4,35 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
+import { Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      error: ''
     };
-  }
+  }  
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    console.log('CDM running');
+    axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      console.log(res);
+      this.setState({ smurfs: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      this.setState({ error: err });
+    });
+  };
+
+
   render() {
     return (
       <div className="App">
